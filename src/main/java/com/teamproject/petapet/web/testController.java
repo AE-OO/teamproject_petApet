0ab17d7970482cbea680ac2feae45c66676f3557
woon1 <woon1998@naver.com>
2022-10-04 21:09:29 +0900

    petApet update v1.0

---
+++ src/main/java/com/teamproject/petapet/web/testController.java
@@ -0,0 +1,13 @@
+package com.teamproject.petapet.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+
+    @GetMapping("/")
+    public String test(){
+        return "index";
+    }
+}
