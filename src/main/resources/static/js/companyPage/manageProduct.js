19b63dba7a7bd7656b80a56e5ec06f127a00f840
woon1 <woon1998@naver.com>
2022-12-16 02:16:45 +0900

    petApet CW branch update v3.8

--- src/main/resources/static/js/companyPage/manageProduct.js
+++ src/main/resources/static/js/companyPage/manageProduct.js
@@ -5,6 +5,23 @@ $(document).ready(function(){
 function getProductList(){
     $.getJSON('/product/manageProduct', function(result){
         var list = '';
+        if(result.length > 0){
+            $.each(result, function(idx, product) {
+                list = `<tr id="productData`+ idx +`">
+                                    <td class="pl-0" id="productId">`+ product.productId +`</td>
+                                    <td>` + product.productName + `</td>
+                                    <td>` + product.productDiv + `</td>
+                                    <td>` + product.productPrice + `</td>
+                                    <td>` + product.productStock + `</td>
+                                    <td>` + product.productStatus + `</td>
+                                    <td>0</td>
+                                    <td>` + product.productReport + `</td>
+                                </tr>`;
 
+                $(".productData").append(list);
+            })
+        }else{
+            $("#noData").text("등록된 상품이 없습니다.");
+        }
     })
 }
