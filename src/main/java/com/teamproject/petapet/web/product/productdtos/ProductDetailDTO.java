448a40f1e24115ff6e2465f837baccf17ccf7324
occocco <wcaqihgn@gmail.com>
2023-02-03 18:39:29 +0900

    petApet update v4.8.2

--- src/main/java/com/teamproject/petapet/web/product/productdtos/ProductDetailDTO.java
+++ src/main/java/com/teamproject/petapet/web/product/productdtos/ProductDetailDTO.java
@@ -3,6 +3,7 @@ package com.teamproject.petapet.web.product.productdtos;
 import com.teamproject.petapet.web.product.fileupload.UploadFile;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.util.List;
 
@@ -15,9 +16,11 @@ public class ProductDetailDTO {
     private List<UploadFile> productImg;
     private String productName;
     private String productSeller;
+    @NumberFormat(pattern = "###,###")
     private Long productPrice;
     private Long productStock;
     private Long productDiscountRate;
+    @NumberFormat(pattern = "###,###")
     private Long productUnitPrice;
     private Long productReviewCount;
 
