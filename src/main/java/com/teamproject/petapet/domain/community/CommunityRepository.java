a19d88573e93a2c61b564698b9ae191614d1b593
Saron Jang <saron312@naver.com>
2023-02-03 14:17:07 +0900

    petApet saron branch update v4.8

--- src/main/java/com/teamproject/petapet/domain/community/CommunityRepository.java
+++ src/main/java/com/teamproject/petapet/domain/community/CommunityRepository.java
@@ -2,6 +2,7 @@ package com.teamproject.petapet.domain.community;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
